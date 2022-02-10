31e1d2675183bbdfd2018d0a0d52a947ab8c508b
wadood <wadoodislam@gmail.com>
2022-02-11 00:42:36 +0500

    Final code for Margala

--- utils.py
+++ utils.py
@@ -12,24 +12,45 @@ import requests
 
 
 # def gstreamer_pipeline(capture_width=640, capture_height=480, display_width=640, display_height=480, framerate=30, flip_method=2):
-def gstreamer_pipeline(capture_width=3280, capture_height=2464, display_width=640, display_height=480, framerate=30, flip_method=2):
+#     return (
+#             "nvarguscamerasrc ! "
+#             "video/x-raw(memory:NVMM), "
+#             "width=(int)%d, height=(int)%d, "
+#             "format=(string)NV12, framerate=(fraction)%d/1 ! "
+#             "nvvidconv flip-method=%d ! "
+#             "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+#             "videoconvert ! "
+#             "video/x-raw, format=(string)BGR ! appsink"
+#             % (
+#                 capture_width,
+#                 capture_height,
+#                 framerate,
+#                 flip_method,
+#                 display_width,
+#                 display_height,
+#             )
+#     )
+def gstreamer_pipeline(capture_width=1920, capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
     return (
-            "nvarguscamerasrc ! "
-            "video/x-raw(memory:NVMM), "
-            "width=(int)%d, height=(int)%d, "
-            "format=(string)NV12, framerate=(fraction)%d/1 ! "
-            "nvvidconv flip-method=%d ! "
-            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-            "videoconvert ! "
-            "video/x-raw, format=(string)BGR ! appsink"
-            % (
-                capture_width,
-                capture_height,
-                framerate,
-                flip_method,
-                display_width,
-                display_height,
-            )
+        "nvarguscamerasrc !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
     )
 
 
