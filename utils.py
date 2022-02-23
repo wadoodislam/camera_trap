f6d1f081c2e7be60e3495c8fe77fc31eb8367951
Muzammal <muzammal0@gmail.com>
2022-02-23 14:58:52 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -37,16 +37,12 @@ def gstreamer_pipeline(capture_width=1280, capture_height=720,
     display_height=540,
     framerate=30,
     flip_method=0,
-    GST_DEBUG=2,
+
 ):
     return (
 
         "nvarguscamerasrc !"
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
+        
         % (
             capture_width,
             capture_height,
@@ -54,7 +50,7 @@ def gstreamer_pipeline(capture_width=1280, capture_height=720,
             flip_method,
             display_width,
             display_height,
-            GST_DEBUG,
+
         )
     )
 
