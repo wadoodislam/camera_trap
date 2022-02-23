696e020534be9e6213da1481aa6d16692c095f1e
Muzammal <muzammal0@gmail.com>
2022-02-23 15:01:00 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,26 +12,6 @@ from datetime import datetime, timedelta
 
 import requests
 
-
-# def gstreamer_pipeline(capture_width=640, capture_height=480, display_width=640, display_height=480, framerate=30, flip_method=2):
-#     return (
-#             "nvarguscamerasrc ! "
-#             "video/x-raw(memory:NVMM), "
-#             "width=(int)%d, height=(int)%d, "
-#             "format=(string)NV12, framerate=(fraction)%d/1 ! "
-#             "nvvidconv flip-method=%d ! "
-#             "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-#             "videoconvert ! "
-#             "video/x-raw, format=(string)BGR ! appsink"
-#             % (
-#                 capture_width,
-#                 capture_height,
-#                 framerate,
-#                 flip_method,
-#                 display_width,
-#                 display_height,
-#             )
-#     )
 def gstreamer_pipeline(capture_width=1280, capture_height=720,
     display_width=960,
     display_height=540,
