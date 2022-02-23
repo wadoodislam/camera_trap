58b82b0f54b38f4863c4abad8641d7a3cb32915b
Muzammal <muzammal0@gmail.com>
2022-02-23 14:54:12 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -38,24 +38,19 @@ def gstreamer_pipeline(capture_width=1280, capture_height=720,
     framerate=30,
     flip_method=0,
     GST_DEBUG=2,
-)
-    # return (
-    #     "GST_DEBUG=2,"
-    #     "nvarguscamerasrc !"
-    #     "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-    #     "nvvidconv flip-method=%d ! "
-    #     "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-    #     "videoconvert ! "
-    #     "video/x-raw, format=(string)BGR ! appsink"
-    #     % (
-    #         capture_width,
-    #         capture_height,
-    #         framerate,
-    #         flip_method,
-    #         display_width,
-    #         display_height,
-    #     )
-    # )
+):
+    return (
+
+        "nvarguscamerasrc !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            
+        )
+    )
 
 
 def dt_parse(t):
