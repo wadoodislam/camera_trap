29b251f6cbf125d8cf495fc0b1030e0de88888d3
muzammal0 <muzammal0@gmail.com>
2022-02-24 11:41:29 +0500

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,4 +1,3 @@
-import json
 import os
 import shutil
 import time
@@ -15,9 +14,11 @@ class UploadManager(Constants):
 
     def send_image(self, event, item, width, height):
         item_path = os.path.join(self.upload_dir, event, item)
+        file_dt = datetime.fromtimestamp(float(item[:-4]) / 1000)
         im = Image.open(item_path)
         # add footer here
-        im = ImageOperations.addFooter(im, file_name_t, '')
+        short_txt, long_txt = self.get_copy_rights(file_dt)
+        #im = ImageOperations.addFooter(im, short_txt, long_txt)
         temp_item_path = os.path.join(self.temp_dir, item)
         file, ext = os.path.splitext(temp_item_path)
         im_resize = im.resize((width, height), Image.ANTIALIAS)
@@ -26,23 +27,20 @@ class UploadManager(Constants):
         files = [('file', (item, open(temp_item_path, 'rb'), 'image/jpg'))]
         try:
             response = requests.request("POST", self.image_url, headers=self.headers_im, data=payload, files=files)
-            file_name_t = datetime.fromtimestamp(float(item[:-4])/1000)
-            if response.status_code != 201:
-                log = {'message': 'Upload failed for Event "{}" & Image Time: "{}" with Status: {}'.format(event, file_name_t, response.status_code)}
-                requests.post(self.logs_url, headers=self.headers, data=json.dumps(log))
-                return False
-            log = {'message': 'Upload success for Event "{}" & Image Time: "{}" with Status: {}'.format(event, file_name_t, response.status_code)}
-            requests.post(self.logs_url, headers=self.headers, data=json.dumps(log))
+            if response.status_code == 201:
+                self.send_log('Upload success for Event "{}" & Image Time: "{}" with'.format(event, file_dt))
+                return True
         except Exception as e:
-            return False
-        return True
+            pass
+
+        self.send_log('Upload failed for Event "{}" & Image Time: "{}" with'.format(event, file_dt))
+        return False
 
     def run(self):
         print('checking for uploads...')
         while True:
-            #if self.should_update:
-            #    self.update()
-            #    print('Updated from dashboard at: {}'.format(datetime.now()))
+            if self.should_update:
+               self.update()
 
             events = os.listdir(self.upload_dir)
             if not events:
@@ -52,7 +50,7 @@ class UploadManager(Constants):
                 items = os.listdir(os.path.join(self.upload_dir, event))
                 if items:
                     item = items[0]
-                    self.send_image(event, item, width=640, height=480) # - Hardcoded
+                    self.send_image(event, item, width=640, height=480)  # - Hardcoded
                     self.move_to_done(event, item)
                 else:
                     shutil.rmtree(os.path.join(self.upload_dir, event))
@@ -65,6 +63,10 @@ class UploadManager(Constants):
         shutil.move(item_path, os.path.join(done_item_path, item))
         os.remove(os.path.join(self.temp_dir, item))
 
+    def get_copy_rights(self, file_dt):
+        long_txt = file_dt.strftime('%b %d, %Y     %H:%M:%S') + '     ' + self.name + '     ' + 'POWERED BY LUMS'
+        shrt_txt = file_dt.strftime('%d.%m.%y  %H:%M') + '  ' + "".join(e[0] for e in self.name.split()) + '  LUMS'
+        return shrt_txt, long_txt
 
 
 if __name__ == "__main__":
