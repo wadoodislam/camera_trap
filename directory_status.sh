7c5fe40177f906c3b51416eebd2dc951a4ca9265
wadood <wadoodislam@gmail.com>
2022-07-08 04:47:04 +0500

    camera_trap.conf

--- directory_status.sh
+++
@@ -1,6 +0,0 @@
-echo Events Directory
-ls -alt data_root/events
-echo Done Directory
-ls -alt data_root/done
-echo Temp Directory
-ls -alt data_root/temp
---
+++ miscellaneous/directory_status.sh
@@ -0,0 +1,6 @@
+echo Events Directory
+ls -alt ../data_root/events
+echo Done Directory
+ls -alt ../data_root/done
+echo Temp Directory
+ls -alt ../data_root/temp
