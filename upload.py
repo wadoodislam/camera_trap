6f46f32bbe199a6e6f4b0366bf5d34a14ff3d69b
muqsitamir <islammuqsit7@gmail.com>
2022-12-13 21:59:44 +0500

    "added keep_4g_on for ssh"

--- upload.py
+++ upload.py
@@ -42,6 +42,10 @@ class UploadManager(Constants):
 
             events = os.listdir(self.events_dir)
             if not events:
+                logging.info(str(self.should_turn_4g_off() and self.is_4g_on and not self.keep_4g_on))
+                logging.info(str(self.should_turn_4g_off()))
+                logging.info(str(self.is_4g_on))
+                logging.info(str(not self.keep_4g_on))
                 if self.should_turn_4g_off() and self.is_4g_on and not self.keep_4g_on:
                     GPIO.output(self.pin_4g, GPIO.HIGH)
                     logging.info("4g turned OFF")
