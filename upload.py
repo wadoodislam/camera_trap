cc40e298e9d5bfd9f76e851101942254df51a69b
muqsitamir <islammuqsit7@gmail.com>
2023-01-18 19:32:13 +0500

    testing 4g v3

--- upload.py
+++ upload.py
@@ -27,8 +27,10 @@ class UploadManager(Constants):
         logging.info(f'Checked Tables')
         self.put_log([f'"{datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}"', '"SCRIPT_STARTED"', '1', f'"Upload Started"'])
         self.setup_sensors()
+        self.bad_request_timer = None
+        self.retry_request_timer = None
         GPIO.output(self.pin, GPIO.LOW)
-        logging.info("4g turned ON")
+        logging.info("4g turned ON at Reboot")
         self.lastuse_4g_at = datetime.now()
         self.is_4g_on = True
 
@@ -42,7 +44,9 @@ class UploadManager(Constants):
                     self.logging = False
             else:
                 self.logging = True
-
+            if self.should_stop_requesting():
+                self.retry_request_timer = datetime.now()
+                self.bad_request_timer = None
             events = os.listdir(self.events_dir)
             if not events:
                 logging.info(str(self.should_turn_4g_off()))
@@ -54,11 +58,19 @@ class UploadManager(Constants):
                     self.is_4g_on = False
                 time.sleep(1)
             else:
+                if not self.should_retry():
+                    if self.is_4g_on:
+                        GPIO.output(self.pin, GPIO.HIGH)
+                        logging.info("4g turned OFF for 15 minutes; event upload unsuccessful")
+                        self.is_4g_on = False
+                    continue
+                if self.should_retry():
+                    if self.retry_request_timer is not None:
+                        self.retry_request_timer = None
                 if not self.is_4g_on:
                     GPIO.output(self.pin, GPIO.LOW)
-                    logging.info("4g turned ON")
+                    logging.info("4g turned ON; event detected")
                     self.is_4g_on = True
-
                 event = sorted(events, key=lambda e: os.stat(os.path.join(self.events_dir, e)).st_ctime, reverse=True)[0]
                 items = os.listdir(os.path.join(self.events_dir, event))
                 if items:
@@ -69,9 +81,8 @@ class UploadManager(Constants):
                                       '"UPLOAD_FAILED"', '1', f'"Corrupted image found! UUID: {event}"'])
                         self.move_to_done(event, item)
                     elif self.send_image(event, item, temp_img_path):
-                        self.lastuse_4g_at = datetime.now()
                         self.move_to_done(event, item)
-
+                    self.lastuse_4g_at = datetime.now()
                     if temp_img_path:
                         os.remove(os.path.join(self.temp_dir, item))
                 else:
@@ -87,12 +98,17 @@ class UploadManager(Constants):
                 self.put_log([f'"{datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}"',
                               '"UPLOAD_SUCCESS"', '1', f'"UUID: {event} & Image At: {file_dt}"'])
                 logging.info(f'Successfully Uploaded Image At: {file_dt}')
-                return True
         except Exception as e:
+            if self.bad_request_timer is None:
+                self.bad_request_timer = datetime.now()
             self.put_log([f'"{datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}"',
                           '"UPLOAD_FAILED"', '1', f'"UUID: {event} & Image At: {file_dt}"'])
             logging.info(f'Not uploaded image At: {file_dt}')
             pass
+        else:
+            if self.bad_request_timer is not None:
+                self.bad_request_timer = None
+            return True
 
         return False
 
@@ -122,6 +138,16 @@ class UploadManager(Constants):
         im_resize = cv2.resize(im, (width, height))
         cv2.imwrite(file + '.jpg', im_resize, [cv2.IMWRITE_JPEG_QUALITY, 90])
         return temp_item_path
+
+    def should_stop_requesting(self):
+        if self.bad_request_timer is None:
+            return False
+        return datetime.now() > self.bad_request_timer + timedelta(seconds=150)
+
+    def should_retry(self):
+        if self.retry_request_timer is None:
+            return True
+        return datetime.now() > self.bad_request_timer + timedelta(seconds=900)
 
     def should_turn_4g_off(self):
         rn = datetime.now()
